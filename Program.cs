8b48f0f15853dc9ab624cbdd41d49df5656ff322
GitDogBar <Dogbar>
2021-08-10 15:56:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DO_Condiion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string select = "";
+
+            do
+            {
+                Console.WriteLine("Here a cute Cat");
+                Console.WriteLine("Do you want to see a Dog? (input yes)");
+                select = Console.ReadLine();
+
+                Console.WriteLine("Here a cute Dog");
+                Console.WriteLine("Do you want to see a Cat? (input yes)");
+                select = Console.ReadLine();
+
+            } while (select == "yes");
+            Console.WriteLine("\n");
+            Console.WriteLine("Good Bye");
+        }
+    }
+}
